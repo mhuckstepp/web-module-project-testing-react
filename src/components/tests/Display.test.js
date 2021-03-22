0a1a9089094d9ba6d262febbd8d07704cedd4340
Max Huckstepp <mhuckstepp@gmail.com>
2021-03-22 16:01:35 -0700

    Complete

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,114 @@
+import React from "react";
+import {
+  getByLabelText,
+  getByTestId,
+  getByText,
+  render,
+  screen,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Display from "../Display";
 
+import mockFetchShow from "../../api/fetchShow";
+jest.mock("../../api/fetchShow");
 
+const getButton = () => {
+  const button = screen.getByRole("button");
+  return button;
+};
 
+test("renders Display without errors", () => {
+  //   arrange
+  render(<Display />);
+});
 
+test("When fetch is called the show component will display", async () => {
+  //   arrange
+  render(<Display />);
+  mockFetchShow.mockResolvedValueOnce({
+    name: "show name",
+    summary: "show summary",
+    seasons: [
+      {
+        id: 5555,
+        name: "season 1",
+        episodes: [],
+      },
+      {
+        id: 5552,
+        name: "season 2",
+        episodes: [],
+      },
+    ],
+  });
 
+  const button = getButton();
 
+  userEvent.click(button);
 
+  // assert
+  const shows = await screen.findByTestId(/show-container/i);
+});
 
+test("When fetch is called the show component will display", async () => {
+  //   arrange
+  render(<Display />);
+  mockFetchShow.mockResolvedValueOnce({
+    name: "show name",
+    summary: "show summary",
+    seasons: [
+      {
+        id: 5555,
+        name: "season 1",
+        episodes: [],
+      },
+      {
+        id: 5552,
+        name: "season 2",
+        episodes: [],
+      },
+    ],
+  });
 
+  const button = getButton();
 
+  userEvent.click(button);
 
+  // assert
+  const shows = await screen.findAllByTestId(/season-option/i);
+  expect(shows).toHaveLength(2);
+});
 
+test("When fetch is called the show component will display", async () => {
+  //   arrange
+  const mockGetData = jest.fn();
+  render(<Display displayFunc={mockGetData} />);
+  mockFetchShow.mockResolvedValueOnce({
+    name: "show name",
+    summary: "show summary",
+    seasons: [
+      {
+        id: 5555,
+        name: "season 1",
+        episodes: [],
+      },
+      {
+        id: 5552,
+        name: "season 2",
+        episodes: [],
+      },
+    ],
+  });
 
+  const button = getButton();
+  userEvent.click(button);
 
+  // assert
+  const shows = await screen.findAllByTestId(/season-option/i);
+  expect(shows).toHaveLength(2);
+  expect(mockGetData).toHaveBeenCalled();
+  expect(mockGetData).toHaveBeenCalledTimes(1);
+});
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
